e8f0820758cfd0c3b07012ebbd2079fc3fb37a0d
markov-avl <70623307+markov-avl@users.noreply.github.com>
2023-01-08 19:35:37 +1000

    Update average.cpp

--- lab1/average.cpp
+++ lab1/average.cpp
@@ -1,22 +1,22 @@
 #include "average.h"
 
-unsigned averageStaticOmp(const unsigned *v, size_t n) {
-    auto sum = 0U;
+double averageStaticOmp(const unsigned *v, size_t n) {
+    auto sum = .0;
 #pragma omp parallel for reduction(+: sum)
     for (size_t i = 0; i < n; ++i) {
         sum += v[i];
     }
 
-    return sum / (unsigned) n;
+    return sum / (double) n;
 }
 
 
-unsigned averageDynamicOmp(const unsigned *v, size_t n) {
-    auto sum = 0U;
+double averageDynamicOmp(const unsigned *v, size_t n) {
+    auto sum = .0;
 #pragma omp parallel for reduction(+: sum) schedule(dynamic)
     for (size_t i = 0; i < n; ++i) {
         sum += v[i];
     }
 
-    return sum / (unsigned) n;
+    return sum / (double) n;
 }
